b5598b5559c31fca642be8210cabbce13a6a22e4
Syam Sankar <97424601+21A91A1262@users.noreply.github.com>
2024-10-21 16:34:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "python Developer", "Web Developer"],
+    typeSpeed : 30,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
